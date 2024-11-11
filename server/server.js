77e37ee0686d10a4db92163caacf24e5dbc36732
Meandering Brook <jaguar_rebuke_0g@icloud.com>
2024-11-11 13:38:28 -0500

    Server Code Correction, Express Static Code Block

--- server/server.js
+++ server/server.js
@@ -207,6 +207,11 @@ const startApolloServer = async () => {
       context: authMiddleware,
     })
   );
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 };
 
 // Starts Apollo Server
